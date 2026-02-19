f4aa8b86247cec53e5b186e2ab39ba0314c40fe5
Riley Langbein <rilrom-dev@protonmail.com>
2026-02-19 20:52:15 +1030

    refactor(lightbox): move svgs to separate files

--- packages/lightbox/src/Lightbox.tsx
+++ packages/lightbox/src/Lightbox.tsx
@@ -25,6 +25,7 @@ import {
 	useRef,
 	useState,
 } from "react";
+import { QuestionMark } from "./components/QuestionMark.js";
 import { LightboxProvider } from "./Lightbox.context.js";
 import classes from "./Lightbox.module.css";
 import { LightboxSlide } from "./LightboxSlide.js";
@@ -255,21 +256,7 @@ export const Lightbox = factory<LightboxFactory>((_props, ref) => {
 									>
 										{thumbnail ?? (
 											<Box {...getStyles("thumbnailPlaceholder")}>
-												<svg
-													aria-hidden="true"
-													xmlns="http://www.w3.org/2000/svg"
-													width="16"
-													height="16"
-													viewBox="0 0 24 24"
-													fill="none"
-													stroke="currentColor"
-													strokeWidth="2"
-													strokeLinecap="round"
-													strokeLinejoin="round"
-												>
-													<path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
-													<path d="M12 17h.01" />
-												</svg>
+												<QuestionMark />
 											</Box>
 										)}
 									</UnstyledButton>
